f075d0559a44ecdc4b9244751349b47d56a37334
HZCJ <luffy.ja@gmail.com>
2016-06-16 10:39:42 +0800

    add testcase

--- src/test/java/com/huotu/wedget/picBanner/MyWidgetTest.java
+++ src/test/java/com/huotu/wedget/picBanner/MyWidgetTest.java
@@ -1,24 +1,34 @@
 package com.huotu.wedget.picBanner;
 
 import com.huotu.widget.test.WidgetTest;
-import org.junit.Test;
+import org.openqa.selenium.WebElement;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import java.util.Map;
+import java.util.function.Supplier;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author CJ
  */
 public class MyWidgetTest extends WidgetTest {
 
-    /**
-     * 编辑器测试
-     */
-    @Test
-    public void editor() throws Exception {
-        mockMvc.perform(get("/editor"))
-                .andDo(print());
+
+    @Override
+    protected boolean printPageSource() {
+        return false;
     }
 
+    @Override
+    protected void editorWork(WebElement editor, Supplier<Map<String,Object>> currentWidgetProperties){
+        editor.click();
 
+        String alert = driver.switchTo().alert().getText();
+        System.out.println(alert);
+
+        Map<String,Object> ps = currentWidgetProperties.get();
+
+        assertThat(currentWidgetProperties.get().get("count"))
+                .isEqualTo(1);
+    }
 }
