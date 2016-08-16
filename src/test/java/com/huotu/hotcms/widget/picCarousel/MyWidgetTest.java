6e3920500ec6dd9e3c8eee49859aa9dbdf0bf88b
lihuaixin <1072931117@qq.com>
2016-08-16 10:39:39 +0800

    添加编辑器浏览测试

--- src/test/java/com/huotu/hotcms/widget/picCarousel/MyWidgetTest.java
+++ src/test/java/com/huotu/hotcms/widget/picCarousel/MyWidgetTest.java
@@ -58,7 +58,8 @@ public class MyWidgetTest extends WidgetTest {
     }
 
     @Override
-    protected void editorBrowseWork(Widget widget, Function<ComponentProperties, WebElement> uiChanger) throws IOException {
+    protected void editorBrowseWork(Widget widget, Function<ComponentProperties, WebElement> uiChanger
+            , Supplier<Map<String, Object>> currentWidgetProperties) throws IOException {
 
     }
 
